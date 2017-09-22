2f9476bed05cfe6b780a1f203723de87741291c5
XPS 15 <XPS 15@mpula>
2017-09-22 11:14:42 +0200

    Merge branch 'master' of https://github.com/micpul/ServiceGenerator.git
    # Conflicts:
    #	src/main/java/pl/mpulas/service/generator/ServiceGenerator.java

--- src/main/java/pl/mpulas/service/generator/ServiceGenerator.java
+++ src/main/java/pl/mpulas/service/generator/ServiceGenerator.java
@@ -23,7 +23,7 @@ public class ServiceGenerator {
 		GeneratorFileUtils.copyCasProjectFolder(sourceCasPath, config); //dddd  hhhhhhh qqqqqq
 		
 		//dddd
-		 //fffffffffffffffffff hujjjj 2 3333 4444 
+		 //fffffffffffffffffff hujjjj 2 3333 4444   
 	}
 	
 	
